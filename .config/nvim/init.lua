7841a70ac3c08be65af616aa50f297fae82d54b4
meanderio <acahn@meanderings.io>
2025-10-15 18:37:38 -0500

    create: lsp configuration plugins added

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,3 +9,9 @@ vim.opt.softtabstop = 2
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
+
+vim.keymap.set("i", "<C-,>", "<C-o>j")
+vim.keymap.set("i", "<C-p>", "<C-o>k")
