1f97461cb51ff014ace0627c57c7bdeac4de1480
meanderio <acahn@meanderings.io>
2025-10-28 21:08:47 -0500

    update: reset on lsp configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,6 @@
 require("mndr")
 require("config.lazy")
+require("config.lsp")
 
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
@@ -9,9 +10,12 @@ vim.opt.softtabstop = 2
 vim.wo.number = true
 vim.wo.relativenumber = true
 
-vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
+vim.opt.winborder = 'rounded'
+
+--vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+--vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+--vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
 
 vim.keymap.set("i", "<C-,>", "<C-o>j")
 vim.keymap.set("i", "<C-.>", "<C-o>k")
+
