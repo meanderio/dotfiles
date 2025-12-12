82d7e384a8f7f67ba5995776acba9e2e09395281
meanderio <acahn@meanderings.io>
2025-12-11 20:23:53 -0600

    update: larger resolution font for terminal

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,23 +1,20 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Catppuccin Mocha"
 
-config.font = wezterm.font(
-  "JetBrainsMono Nerd Font Mono",
-  {
-    weight="Medium",
-    stretch="Normal",
-    style="Normal"
-  }
-)
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono", {
+	weight = "Medium",
+	stretch = "Normal",
+	style = "Normal",
+})
 config.window_padding = {
-  left = 4.0,
-  right = 4.0,
-  top = 2.0,
-  bottom = 2.0
+	left = 4.0,
+	right = 4.0,
+	top = 2.0,
+	bottom = 2.0,
 }
-config.font_size = 14.0
+config.font_size = 16.0
 config.use_fancy_tab_bar = false
 config.default_cursor_style = "BlinkingUnderline"
 config.tab_bar_at_bottom = true
@@ -25,50 +22,49 @@ config.tab_bar_at_bottom = true
 
 local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
 tabline.setup({
-  options = {
-    theme = "Catppuccin Mocha"
-  }
+	options = {
+		theme = "Catppuccin Mocha",
+	},
 })
 
-config.leader = { key = 'i', mods = 'CTRL' }
+config.leader = { key = "i", mods = "CTRL" }
 
 config.keys = {
-  {
-    key = 'w',
-    mods = 'CTRL',
-    action = wezterm.action.CloseCurrentPane { confirm = false },
-  },
-  {
-    key = 'h',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.ActivatePaneDirection('Left')
-  },
-  {
-    key = 'j',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.ActivatePaneDirection('Down')
-  },
-  {
-    key = 'k',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.ActivatePaneDirection('Up')
-  },
-  {
-    key = 'l',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.ActivatePaneDirection('Right')
-  },
-  {
-    key = 'd',
-    mods = 'LEADER',
-    action = wezterm.action.SplitVertical
-  },
-  {
-    key = 'f',
-    mods = 'LEADER',
-    action = wezterm.action.SplitHorizontal
-  },
+	{
+		key = "w",
+		mods = "CTRL",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	{
+		key = "h",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "j",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "k",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "l",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "d",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical,
+	},
+	{
+		key = "f",
+		mods = "LEADER",
+		action = wezterm.action.SplitHorizontal,
+	},
 }
 
 return config
-
